5aac9904ccc2b7418ce95b0df4cdf84d8e26581b
Adam Featherston <j.adam.featherston@gmail.com>
2022-11-29 15:58:53 -0600

    ex 7

--- invoice_totals.sql
+++ invoice_totals.sql
@@ -1,7 +1,9 @@
 -- SQLite
-SELECT Employee.FirstName, Employee.LastName, InvoiceId
-FROM EMPLOYEE
+SELECT Employee.FirstName||" "||Employee.LastName AS "Employee_FullName", Customer.FirstName||" "||Customer.LastName AS "Customer_FullName", Customer.Country AS Customer_Country, Total AS Total_Customer_Purchases
+FROM Employee
 INNER JOIN Customer
     ON Employee.EmployeeId = Customer.SupportRepId
 INNER JOIN Invoice
     ON Customer.CustomerId = Invoice.CustomerId;
+
+
