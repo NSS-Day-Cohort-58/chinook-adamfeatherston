039c439a1063c3d629913d721f174d7f109169c2
Adam Featherston <j.adam.featherston@gmail.com>
2022-11-30 09:50:47 -0600

    exercise 9

--- total_sales_{year}.sql
+++ total_sales_{year}.sql
@@ -1,12 +1,12 @@
-SELECT Count(*) AS "2009 and 2011 Number of Invoices"
+SELECT SUM(Total) AS "2009 and 2011 Total Sales"
 FROM Invoice
 WHERE InvoiceDate LIKE '%2009%'
 OR InvoiceDate LIKE '%2011%';
 
-SELECT Count(*) AS "2009 Number of Invoices"
+SELECT SUM(Total) AS "2009 Total Sales"
 FROM Invoice
 WHERE InvoiceDate LIKE '%2009%';
 
-SELECT Count(*) AS "2011 Number of Invoices"
+SELECT SUM(Total) AS "2011 Total Sales"
 FROM Invoice
 WHERE InvoiceDate LIKE '%2011%';
