f942730e2aea9034297c410526f80a793a6ff76a
Adam Featherston <j.adam.featherston@gmail.com>
2022-12-01 10:54:45 -0600

    exercise 19

---
+++ top_2009_agent.sql
@@ -0,0 +1,16 @@
+SELECT Employee.FirstName||" "||Employee.LastName AS "Employee_FullName", SUM (Invoice.Total) AS "Employee_Total_Sales"
+FROM Invoice
+JOIN Customer
+    ON Invoice.CustomerId = Customer.CustomerId
+JOIN Employee
+    ON Customer.SupportRepId = Employee.EmployeeId
+WHERE InvoiceDate LIKE "%2009%"
+GROUP BY Employee.EmployeeId
+ORDER BY Max(Invoice.Total) DESC
+LIMIT 1;
+
+
+
+
+Which sales agent made the most in sales in 2009?
+HINT: Use the MAX function on a subquery.
